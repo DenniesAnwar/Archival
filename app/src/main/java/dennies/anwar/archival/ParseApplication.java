62df12c347d7e05077a228b385601cd12cada2c3
Dennies Anwar <dennies.anwar34@myhunter.cuny.edu>
2021-03-18 16:23:26 -0400

    Successful posting and saving into back-end server

--- app/src/main/java/dennies/anwar/archival/ParseApplication.java
+++ app/src/main/java/dennies/anwar/archival/ParseApplication.java
@@ -1,4 +1,23 @@
 package dennies.anwar.archival;
 
-public class ParseApplication {
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("6i6aE9d0BNgU30v778FSzxsbw6R4wSoIQBxyDzWy")
+                .clientKey("4pYAUWgPQYzegHE2UXE3Yb8YMBH6MDwE6F1ZMlBC")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
 }
